2aa408bdb822c82142e9d318a019a5db664d7e8d
sushil-sah <139619886+sushil-sah@users.noreply.github.com>
2025-10-03 11:08:59 +1000

    created class AIApp for model Imagetotext and texttoimage

--- gui_components.py
+++ gui_components.py
@@ -3,3 +3,16 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
 from models import ImageToText, TextToImage
+
+class AIApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("AI Tkinter GUI")
+        self.geometry("800x600")
+
+        # Models
+        self.image_to_text = ImageToText()
+        self.text_to_image = TextToImage()
+
+        # Widgets
+        self.create_widgets()
