1603cc26d8e70cd4dde2d688dad3b7393b1ca0ce
sushil-sah <139619886+sushil-sah@users.noreply.github.com>
2025-10-03 11:06:07 +1000

    importing header files for gui components

--- gui_components.py
+++ gui_components.py
@@ -1,1 +1,5 @@
 # this file will have the GUI interface functions
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
+from models import ImageToText, TextToImage
